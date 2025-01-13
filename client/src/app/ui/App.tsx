d8a8783d4fa37744f27929b4e1140bc01dc9e42a
iveflava <tobokov@icloud.com>
2025-01-13 23:04:06 +0300

    fix axios base url

--- client/src/app/ui/App.tsx
+++ client/src/app/ui/App.tsx
@@ -9,7 +9,7 @@ import Loader from '@/shared/ui/Loader/Loader';
 import { fetchAuthTokenVerifyAsyncThunk, fetchProfileAsyncThunk } from '../model/GlobalAsyncThunk';
 import { selectGlobalStatePending } from '../model/GlobalSelectors';
 
-axios.defaults.baseURL = 'http://95.214.62.135:3001';
+axios.defaults.baseURL = 'http://95.214.62.135:3000';
 
 const App = () => {
   const navigate = useNavigate();
